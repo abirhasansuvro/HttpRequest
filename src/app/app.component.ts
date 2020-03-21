5349e3adc3800eae92f3b6e2823af4bbc21b94a4
unknown <abirorton@gmail.com>
2020-03-21 13:49:10 +0600

    Added facility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import { Post } from './post.model';
-import { PostsService } from './posts.service';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
+import { PostsService } from "./posts.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
-  isFetching:boolean=false;
+  isFetching: boolean = false;
+  errorMsg: string = null;
 
-  constructor(private http: HttpClient,private postsService:PostsService) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.isFetching=true;
+    this.isFetching = true;
     this.postsService.fetchPosts().subscribe(
-      returnedData=>{
-        this.loadedPosts=returnedData;
+      returnedData => {
+        this.loadedPosts = returnedData;
+      },
+      error => {
+        this.errorMsg = error.message;
       }
     );
-    this.isFetching=false;
+    this.isFetching = false;
   }
 
   onCreatePost(postData: Post) {
@@ -31,19 +35,21 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    this.isFetching=true;
+    this.isFetching = true;
     // Send Http request
     this.postsService.fetchPosts().subscribe(
-      returnedData=>{
-        this.loadedPosts=returnedData;
+      returnedData => {
+        this.loadedPosts = returnedData;
+      },
+      error => {
+        this.errorMsg = error.message;
       }
     );
-    this.isFetching=false;
+    this.isFetching = false;
   }
 
   onClearPosts() {
     // Send Http request
     this.postsService.deletePosts();
   }
-
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,41 +1,57 @@
-import { Injectable } from '@angular/core';
-import { Post } from './post.model';
-import { HttpClient } from '@angular/common/http';
-import{map} from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { map, catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
-@Injectable({providedIn:'root'})
-export class PostsService{
-    constructor(private http:HttpClient){}
+@Injectable({ providedIn: "root" })
+export class PostsService {
+  constructor(private http: HttpClient) {}
 
-    createAndStorePosts(postData:Post){
-        this.http
-      .post<{name:string}>(
-        'https://http-angular-firebase.firebaseio.com/posts.json',
-        postData
+  createAndStorePosts(postData: Post) {
+    this.http
+      .post<{ name: string }>(
+        "https://http-angular-firebase.firebaseio.com/posts.json",
+        postData,
+        {
+          observe: "response"
+        }
       )
       .subscribe(responseData => {
         console.log(responseData);
       });
-    }
+  }
 
-    fetchPosts(){
-        return this.http.get<{[key:string]:Post}>('https://http-angular-firebase.firebaseio.com/posts.json')
-        .pipe(
-            map(
-            responseData=>{
-                let rsponseArr:Post[]=[];
-                for (let r in responseData)rsponseArr.push({...responseData[r],id:r});
-                return rsponseArr;
-            }
-            )
-        );
-    }
+  fetchPosts() {
+    return this.http
+      .get<{ [key: string]: Post }>(
+        "https://http-angular-firebase.firebaseio.com/posts.json",
+        {
+          headers: new HttpHeaders({
+            Ip: "192.0.0.1"
+          }),
+          params: new HttpParams().set("print", "pretty")
+        }
+      )
+      .pipe(
+        map(responseData => {
+          let rsponseArr: Post[] = [];
+          for (let r in responseData)
+            rsponseArr.push({ ...responseData[r], id: r });
+          return rsponseArr;
+        }),
+        catchError(errorResponse => {
+          console.log("Inside service: " + errorResponse);
+          return throwError(errorResponse);
+        })
+      );
+  }
 
-    deletePosts(){
-        this.http.delete('https://http-angular-firebase.firebaseio.com/posts.json').subscribe(
-            successData=>{
-                console.log(successData);
-            }
-        );
-    }
+  deletePosts() {
+    this.http
+      .delete("https://http-angular-firebase.firebaseio.com/posts.json")
+      .subscribe(successData => {
+        console.log(successData);
+      });
+  }
 }
